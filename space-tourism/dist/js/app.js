c7d5a398fb29f59c47ae9d54611f5e47d56d7f8b
Andree Roudon <andree_2000@hotmail.fr>
2023-04-20 18:12:06 +0200

    responsive all pages

---
+++ space-tourism/dist/js/app.js
@@ -0,0 +1,5 @@
+const btnExplore = document.querySelector(".btn-explore");
+
+btnExplore.addEventListener('click', () => {
+    window.location.href = "https://andreeroudon.github.io/space-tourism/pages/destination-moon.html";
+});
