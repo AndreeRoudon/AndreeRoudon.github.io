9052b6e03a1de42fb09b46cc3a239051de22122f
Andree Roudon <andree_2000@hotmail.fr>
2023-04-22 20:56:00 +0200

    burger responsive

--- space-tourism/dist/js/app.js
+++ space-tourism/dist/js/app.js
@@ -1,5 +1,31 @@
+// bouton Explore vers la page destination 
+
 const btnExplore = document.querySelector(".btn-explore");
 
 btnExplore.addEventListener('click', () => {
     window.location.href = "https://andreeroudon.github.io/space-tourism/pages/destination-moon.html";
 });
+
+// burger animation
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links a')
+
+    burger.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+    
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
