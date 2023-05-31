426630430358ec4639729dba7e1ab63b384560f7
Andree Roudon <andree_2000@hotmail.fr>
2023-05-31 21:49:51 +0200

    cv modifié

--- space-tourism/dist/js/app.js
+++ space-tourism/dist/js/app.js
@@ -3,7 +3,7 @@
 const btnExplore = document.querySelector(".btn-explore");
 
 btnExplore.addEventListener('click', () => {
-    window.location.href = "https://andreeroudon.github.io/space-tourism/pages/destination-moon.html";
+    window.location.href = "https://andreeroudon.github.io/space-tourism/destination-moon.html";
 });
 
 // burger animation
