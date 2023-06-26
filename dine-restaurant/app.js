e888c919b6a93cbb31730218960a9a2fcf26f11a
Andree Roudon <andree_2000@hotmail.fr>
2023-06-26 12:50:03 +0200

    Upgrade dine-restaurant js

--- dine-restaurant/app.js
+++ dine-restaurant/app.js
@@ -1,29 +1,57 @@
-const btn = {
-    1:document.getElementById('btn-1'),
-    2:document.getElementById('btn-2'),
-    3:document.getElementById('btn-3'),
+// Sélectionnez les éléments des boutons
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+
+const images = document.querySelector(".images");
+const titles = document.querySelectorAll("[data-title-slide]");
+const paragraph = document.querySelectorAll("[data-text-slide]");
+const btn = document.querySelectorAll("[data-btn-slide]");
+
+// Définir l'état initial
+images.dataset.imageSlide = "1";
+titles[0].classList.add("active");
+paragraph[0].classList.add("active");
+btn[0].classList.add("active");
+// Fonction pour gérer le clic sur les boutons
+function handleButtonClick(imageSlideIndex) {
+    // Selection de l'image
+    images.dataset.imageSlide = imageSlideIndex;
+    // Selection du tittre
+    titles.forEach((title, index) => {
+        if (index === imageSlideIndex - 1) {
+            title.classList.add("active");
+        } else {
+            title.classList.remove("active");
+        }
+    });
+    // Selection du paragraphe
+    paragraph.forEach((elem, index) => {
+        if (index === imageSlideIndex - 1) {
+            elem.classList.add("active");
+        } else {
+            elem.classList.remove("active");
+        }
+    });
+    // Selection du bouton
+    btn.forEach((bouton, index) => {
+        if (index === imageSlideIndex - 1) {
+            bouton.classList.add("active");
+        } else {
+            bouton.classList.remove("active");
+        }
+    });
 }
 
-
-const slidesImages = {
-    1:document.querySelector('[data-image-slide="1"]'),
-    2:document.querySelector('[data-image-slide="2"]'),
-    3:document.querySelector('[data-image-slide="3"]'),
-}
-
-slidesImages[1].classList.add('active');
-
-btn[1].addEventListener("click", () => {
-    btn[1].classList.toggle('active');
-    slidesImages[1].classList.toggle('active');
+// Ajouter des gestionnaires d'événements aux boutons
+btn1.addEventListener("click", function () {
+    handleButtonClick(1);
 });
 
-btn[2].addEventListener("click", () => {
-    btn[2].classList.toggle('active');
-    slidesImages[2].classList.toggle('active');
+btn2.addEventListener("click", function () {
+    handleButtonClick(2);
 });
 
-btn[3].addEventListener("click", () => {
-    btn[3].classList.toggle('active');
-    slidesImages[3].classList.toggle('active');
+btn3.addEventListener("click", function () {
+    handleButtonClick(3);
 });
